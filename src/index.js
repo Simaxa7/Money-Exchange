86e24849034be6092063b844265ba63593263059
“Simaxa7” <simaxa7@mail.ru>
2018-09-13 15:25:17 +0300

    task100Complited

--- src/index.js
+++ src/index.js
@@ -1,5 +1,93 @@
 // PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+module.exports =  function makeExchange(currency) {
+    var minCoins={};
+ //    // currency - входной параметр в центах
+ //    // H, 	Q, 		D, 		N 	 P.
+ //    // 50¢, 25¢, 	10¢,	5¢   1¢
+	// if (currency==0){ return minCoins };
+	if (currency >= 10000){ 
+		
+		minCoins.error = `You are rich, my friend! We don't have so much coins for exchange`;
+		
+		return minCoins;
+	}
+
+	if (currency >= 50){
+		
+		minCoins.H = 0;
+		while (currency >= 50){
+			if (currency >= 50){
+				currency = currency - 50;
+				minCoins.H = minCoins.H + 1;
+			}
+			
+			if (currency<50) {
+				break;
+			}
+		}
+	}
+
+	if (currency < 50 && currency >= 25){
+		
+		minCoins.Q = 0;
+		
+		while (currency >= 25){
+			
+			if (currency >= 25){
+				currency = currency - 25;
+				minCoins.Q = minCoins.Q + 1;
+			}
+			
+			if (currency < 25){
+				break;
+			}
+		}
+	}
+
+	if (currency <25 && currency >= 10){
+		minCoins.D = 0;
+
+		while (currency >= 10){
+
+			if (currency >= 10){
+				currency = currency - 10;
+				minCoins.D = minCoins.D + 1;
+			}
+
+			if (currency < 10){
+				break;
+			}
+		}
+	}	
+
+	if (currency < 10 && currency >= 5){
+		minCoins.N = 0;
+		
+		while(currency >= 5){
+			
+			if(currency >= 5){
+				currency = currency - 5;
+				minCoins.N = minCoins.N + 1;
+			}
+			if (currency<5){
+				break;
+			}
+		}
+	} 
+
+	if (currency < 5 && currency >= 1){
+		minCoins.P = 0;
+
+		while (currency >= 1){
+			
+			if (currency >= 1){
+				currency = currency - 1;
+				minCoins.P = minCoins.P + 1;
+			}
+			if (currency < 1){
+				break;
+			}
+		}
+	}	
+	return minCoins
 }
